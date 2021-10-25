7d507e5d526acebb4aa330217d369b36fffb840f
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-10-25 10:11:08 -0500

    seed park data, csv

--- db/migrate/20211025135413_create_national_parks.rb
+++ db/migrate/20211025135413_create_national_parks.rb
@@ -2,10 +2,10 @@ class CreateNationalParks < ActiveRecord::Migration[6.1]
   def change
     create_table :national_parks do |t|
       t.string :name
-      t.string :image_url
+      t.text :image_url
       t.string :location
       t.datetime :date_established
-      t.float :area
+      t.string :area
       t.text :description
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
+require 'csv'
+
+puts "seeding parks..."
+
+CSV.foreach('parks.csv') do |row|
+    name = row[0]
+    image_url = row[1]
+    location = row[2]
+    date_established = row[3]
+    area = row[4]
+    description = row[5]
+    NationalPark.create(name: name, image_url: image_url, location: location, date_established: date_established, area: area, description: description)
+end
+
+puts "done seeding"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
