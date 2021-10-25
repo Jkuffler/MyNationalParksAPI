0072e32340f214ad965fde98b35a0c459c3f76c1
Jason <jakuffler@gmail.com>
2021-10-25 15:14:23 -0400

    psuedo code for parklist

---
+++ client/src/ParkList.js
@@ -0,0 +1,14 @@
+
+function ParkList () {
+
+    const park = () => {
+        // for each park we display the name inside a stylized button
+    }
+
+    return
+    <div>
+        <ul>
+            <button type="button" className="parkList">`${park}`</button>
+        </ul>
+    </div>
+}
