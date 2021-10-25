4e749d954ac7c81969bae2d63939a422d215b1b0
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-10-25 16:41:31 -0500

    added thumbnail

--- client/src/FeaturedPark.js
+++ client/src/FeaturedPark.js
@@ -1,9 +1,6 @@
 import {useState} from 'react'
 
-function FeaturedPark({park}) {
-    
-
-    // console.log(parkObj)
+function FeaturedPark({ park }) {
     
     return (
 
@@ -11,12 +8,11 @@ function FeaturedPark({park}) {
     <h2>{park.name}</h2>
     <img src={park.image_url} style={{maxWidth: "400px"}}/>
     <p>{park.description}</p>
-    <p>{park.location}</p>
-    <p>{park.date_established}</p>
-    <p>{park.area}</p>
+    <p><b>Location:</b> {park.location}</p>
+    <p><b>Date Established:</b> {park.date_established}</p>
+    <p><b>Size:</b> {park.area} acres</p>
     {/* <p>{park.rating}</p> */}
 </div>
-
 
     )
 }
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -6,10 +6,16 @@ function NavBar() {
         <div>
             <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand>My National Parks</Navbar.Brand>
+                <Navbar.Brand>
+                    <img 
+                        src="https://upload.wikimedia.org/wikipedia/commons/9/97/Logo_of_the_United_States_National_Park_Service.svg"
+                        style={{maxWidth: "20px", margin: "7px"}}
+                        />
+                    My National Parks
+                </Navbar.Brand>
                 <Nav>
                 <Nav.Link >
-                    My Parks
+                    My Passport
                 </Nav.Link>
                 <Nav.Link href="/roasters">
                     All Parks
