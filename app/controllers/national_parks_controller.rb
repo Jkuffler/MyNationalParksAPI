fc58f5656cf90d62b8e0127f8e3ea846bf1ce340
Jason <jakuffler@gmail.com>
2021-10-25 11:43:04 -0400

    np show index routes in place

--- app/controllers/national_parks_controller.rb
+++ app/controllers/national_parks_controller.rb
@@ -1,2 +1,16 @@
 class NationalParksController < ApplicationController
+    def index
+        national_parks = NationalPark.all
+        render json: national_parks
+    end
+
+    def show
+        np = NationalPark.find_by(id: params[:id])
+        if np
+            render json: np
+        else
+            render json: {error: "Park not found"}, status: :not_found
+        end
+    end
+
 end
