f7a69fb877ae91f1f3ba832e078e3a9712354f51
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-10-28 11:08:06 -0500

    update form fetch

--- client/src/VisitCard.js
+++ client/src/VisitCard.js
@@ -9,11 +9,38 @@ function VisitCard({ visit }) {
     const [formData, setFormData] = useState({ 
         date: visit.date,
         description: visit.description,
-        image_url: visit.national_park.image_url
+        // image_url: visit.national_park.image_url
     })
+
+
 
     function handleOnChange(e) {
         setFormData({...formData, [e.target.name]:e.target.value})
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const headers = {
+            method: "PATCH",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(formData)
+        }
+        fetch(`/visits/${visit.id}`, headers)
+        .then(resp => {
+            if (resp.ok) {
+                resp.json()
+                .then(visit => console.log(visit))
+                setFormToggle(!formToggle)
+                setFormData({
+                    date: visit.date,
+                    description: visit.description,
+                    // image_url: visit.national_park.image_url
+                })
+            } else {
+                resp.json()
+                    .then(errors => alert(errors.error))
+            }
+        })
     }
 
     return(
@@ -26,7 +53,7 @@ function VisitCard({ visit }) {
         </Card.Header>
         <Card.Img variant="top" src={visit.national_park.image_url} style={{maxHeight: '200px', objectFit: 'contain', padding: '10px'}}/>
         <Card.Body>     
-        <Card.Text><b>Date visited:</b> {visit.date}</Card.Text>
+        <Card.Text><b>Date visited:</b> {formData.date}</Card.Text>
         </Card.Body>
         <Card.Body>     
         <Card.Text><b>Description:</b> {visit.description}</Card.Text>
@@ -36,19 +63,19 @@ function VisitCard({ visit }) {
     </CardGroup>
     :
         <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="FormInput">
                 <input type="date" onChange={handleOnChange} placeholder={visit.date} name="date" value={formData.date}/>                
             </div>
             <div className="FormInput">
                 <input type="text" onChange={handleOnChange} placeholder={visit.description} name="description" value={formData.description}/>                
             </div>
-            <div className="FormInput">
+            {/* <div className="FormInput">
                 <input type="text" onChange={handleOnChange} placeholder={visit.image_url} name="image_url" value={formData.image_url}/>                
-            </div>
+            </div> */}
                 <button className="SubmitBtn">Update Visit</button>
         </form>
-        <button onClick={() => setFormToggle(!formToggle)}>Back</button>
+        <button>Back</button>
         </>
     }
     </>
