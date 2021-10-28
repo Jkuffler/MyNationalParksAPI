99ba9057fe5475d1e43d6543cc3edd0b0a825102
Jason <jakuffler@gmail.com>
2021-10-28 09:55:25 -0400

    testing form

--- client/src/CardContainer.js
+++ client/src/CardContainer.js
@@ -1,13 +1,59 @@
 import ParkList from "./ParkList"
 import VisitCard from "./VisitCard"
-function CardContainer({visits, parks, handleClick, currentUser}){
+import {useState} from 'react'
 
-    const visitCards = visits.map(v => <VisitCard key={v.id} visit={v} currentUser={currentUser}/>)
+
+function CardContainer({visits, parks, handleClick, currentUser}) {
+    
+    const visitCards = visits.map(v => <VisitCard key={v.id} visit={v} />)
+    const [formData, setFormData] = useState({
+        user_id: currentUser,
+        national_park: "",
+        date: "",
+        description: ""
+    })
+
+    function handleOnChange(e) {
+        setFormData({...formData, [e.target.name]:e.target.value})
+    }
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        if(formData.national_park === "" || formData.date === "" || formData.description === "") return;
+        const headers = {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(formData)
+        }
+
+        fetch('/visits', headers)
+        .then(resp => resp.json())
+        .then(data => {
+            setFormData({
+                user_id: currentUser,
+                national_park: "",
+                date: "",
+                description: ""
+            })
+        })
+    }
     return(
     <div>
         <ParkList parks={parks} handleClick={handleClick}/>
         {visitCards}
+        <form>
+            <div className="FormInput">
+                <input type="text" placeholder="National Park" onChange={handleOnChange} name="national_park" value={formData.national_park}/>
+            </div>
+            <div className="FormInput">
+                <input type="date" onChange={handleOnChange} placeholder="Date" name="date" value={formData.date}/>                
+            </div>
+            <div className="FormInput">
+                <input type="text" onChange={handleOnChange} placeholder="Description" name="description" value={formData.description}/>                
+            </div>
+                <button className="SubmitBtn" onClick={handleOnSubmit}>Add Visit</button>
+        </form>
     </div>
+    
     )
 }
 
--- client/src/VisitCard.js
+++ client/src/VisitCard.js
@@ -1,39 +1,10 @@
 import { Button, Card, CardGroup } from 'react-bootstrap'
-import {useState} from 'react'
 
-function VisitCard({visit, currentUser}) {
-    const [formData, setFormData] = useState({
-        user_id: currentUser,
-        national_park: "",
-        date: "",
-        description: ""
-    })
 
-    function handleOnChange(e) {
-        setFormData({...formData, [e.target.name]:e.target.value})
-    }
-    const handleOnSubmit = (e) => {
-        e.preventDefault()
-        if(formData.national_park === "" || formData.date === "" || formData.description === "") return;
-        const headers = {
-            method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(formData)
-        }
+function VisitCard({visit}) {
 
-        fetch('/visits', headers)
-        .then(resp => resp.json())
-        .then(data => {
-            setFormData({
-                user_id: currentUser,
-                national_park: "",
-                date: "",
-                description: ""
-            })
-        })
-    }
     return(
-
+    <>
     <CardGroup style={{ padding:'10px'}}>
     <Card style={{ width: '18rem', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', hover: {color: 'blue'} }} className="gear-card"  >
         <Card.Header>
@@ -48,19 +19,8 @@ function VisitCard({visit, currentUser}) {
         </Card.Body>
         </Card>
         </CardGroup>
-    //     <form>
-        //     <div className="FormInput">
-        //         <input type="text" placeholder="National Park" onChange={handleOnChange} name="national park" value={formData.national_park}/>
-        //     </div>
-        //     <div className="FormInput">
-        //         <input type="date" onChange={handleOnChange} placeholder="Date" name="date" value={formData.date}/>                
-        //     </div>
-    //         <div className="FormInput">
-        //         <input type="text" onChange={handleOnChange} placeholder="Description" name="description" value={formData.description}/>                
-        //      </div>
-        //          <button className="SubmitBtn" onClick={handleOnSubmit}>Add Visit</button>
-        // </form>
-        // 
+    
+    </>
         )
     
 }
